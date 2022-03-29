04b863f7b95156b83ba1a14be9f1089f1008d720
Cassandra <cassandrabunschoten@gmail.com>
2022-03-29 09:44:12 +0200

    edited aoc 2021; 2019 day 1 and 2

---
+++ fps/src/main/scala/aoc2019_2.scala
@@ -0,0 +1,55 @@
+package AOC2019
+
+import scala.io.Source
+
+sealed abstract class OpCode(code: Int)
+case object Halt extends OpCode(99)
+sealed abstract class Inst(code: Int, loc1: Int, loc2: Int, loc3: Int) extends OpCode(code)
+case class Add(loc1: Int, loc2: Int, loc3: Int) extends Inst(1, loc1, loc2, loc3)
+case class Mul(loc1: Int, loc2: Int, loc3: Int) extends Inst(2, loc1, loc2, loc3)
+
+case class CPU(memory: List[Int], counter: Int = 0, halted: Boolean = false):
+  def run: Int =
+    step match
+      case CPU(m, c, true) => m(0)
+      case cpu: CPU        => cpu.run
+
+  def step: CPU =
+    program match
+      case Add(l1, l2, l3) => exec(l1, l2, l3, _ + _)
+      case Mul(l1, l2, l3) => exec(l1, l2, l3, _ * _)
+      case Halt            => copy(halted = true)
+
+  def exec(l1: Int, l2: Int, l3: Int, f: (Int, Int) => Int): CPU =
+      val v1 = memory(l1)
+      val v2 = memory(l2)
+      copy(memory = memory.updated(l3, f(v1, v2)), counter = counter + 1)
+
+  def program: OpCode =
+    val List(code, c1, c2, c3) = memory.grouped(4).toList(counter)
+    code match
+      case 1  => Add(c1, c2, c3)
+      case 2  => Mul(c1, c2, c3)
+      case 99 => Halt
+
+object AOC2019_2 extends App:
+
+  val cpu: CPU =
+    CPU(
+      Source
+        .fromFile("fps/src/main/resources/input2019_2.txt")
+        .mkString
+        .split(",")
+        .map(_.toInt)
+        .toList)
+
+  val output = (for{
+    noun    <- Range(0, 99, 1)
+    verb    <- Range(0, 99, 1)
+    updated  = cpu.copy(memory = cpu.memory.updated(1, noun))
+    finalv   = updated.copy(memory = updated.memory.updated(2, verb))
+  } yield (noun, verb, finalv.run))
+  .filter((x, y, z) => z == 19690720)
+
+  println(output)
+        
--- fps/src/main/scala/aoc52020.scala
+++ fps/src/main/scala/aoc52020.scala
@@ -8,6 +8,11 @@ def findSeat(s: String): (Int, Int) =
   val column = find((0,7),s.substring(7).toCharArray.toList)
   (row, column)
 
+def findSeat2(s: String): (Int, Int) = 
+  val row = find2((0,127),s.substring(0,7).toCharArray.toList, 'F', 'B')
+  val column = find2((0,7),s.substring(7).toCharArray.toList, 'L', 'R')
+  (row, column)
+
 def find(l: (Int, Int), s: List[Char]): Int =
   l match 
     case(min, max) =>
@@ -17,11 +22,30 @@ def find(l: (Int, Int), s: List[Char]): Int =
         case ('F' | 'L') :: tail  => find((min, max - ((max - min) / 2)-1), tail)
         case ('B' | 'R') :: tail  => find((min + ((max - min) / 2)+1, max), tail)
         case _            => -1
+
+def find2(l: (Int, Int), s: List[Char], down: Char, up: Char): Int =
+  l match 
+    case(min, max) =>
+      s match
+        case `down` :: Nil    => min
+        case `up` :: Nil      => max
+        case `down` :: tail   => find2((min, max - ((max - min) / 2)-1), tail, down, up)
+        case `up` :: tail     => find2((min + ((max - min) / 2)+1, max), tail, down, up)
+        case _                => -1        
+
 object AOC52020 extends App:
    println(Source
     .fromFile("fps/src/main/resources/input52020.txt")
     .getLines
     .map(findSeat)
+    .map(_ match{ case (row, column) => row * 8 + column})
+    .toList.max
+    )
+
+   println(Source
+    .fromFile("fps/src/main/resources/input52020.txt")
+    .getLines
+    .map(findSeat2)
     .map(_ match{ case (row, column) => row * 8 + column})
     .toList.max
     )
